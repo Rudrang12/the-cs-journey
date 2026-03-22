fe7ff7927f229b93126d23fcfe576c3201b91003
Rudrang12 <rudrang.biswal12@gmail.com>
2026-03-23 03:16:48 +0530

    sol: completed mit 6.100l pset 0 (power and log2)

---
+++ year-1-foundations/mit-6-100l-python/problem-sets/ps0.py
@@ -0,0 +1,20 @@
+                                # -------------PROBLEM SET_0-----------#
+ 
+#GOAL-->To write a python code which takes two numbers as user inputs,
+#       stores them in variables x and y, and performs the following operations
+#           1.x^y [i.e. x raised to the power y]
+#           2.log2(x) [i.e. log(base2) of x] 
+#         
+
+import numpy    # Importing the module numpy
+
+
+x = int(input("enter a number: "))    #Taking the user's input for a number x in an integeral form
+
+y = int(input("enter a number: "))    #Taking the user's input for a number y in an integeral form
+
+
+
+print("x^y is", x**y)    #Printing the result of the operation x**y [i.e. x raised to the power y]
+
+print("log(base2) of x is:",numpy.log2(x))   #Printing the result of the operation log(base2) of x
